d8c69ae8aad431a60280c47e5942ea3b686548b2
melaniegrigg <melanie.grigger@gmail.com>
2020-07-08 15:08:58 -0400

    complete all RESTful routes.all working

--- app/controllers/api/wines_controller.rb
+++ app/controllers/api/wines_controller.rb
@@ -10,5 +10,35 @@ class Api::WinesController < ApplicationController
     render 'show.json.jb'
   end
 
-  
+  def create
+    @wine = Wine.new(
+      id: params[:id],
+      vintner: params[:vintner],
+      varietal: params[:varietal],
+      vintage: params[:vintage],
+      pairings: params[:pairings],
+      region: params[:region],
+      tasting_notes: params[:tasting_notes]
+      )
+      @wine.save
+      render 'show.json.jb'
+  end
+
+  def update
+    @wine = Wine.find_by(id: params[:id])
+    @wine.varietal = params[:varitel] || @wine.varietal
+    @wine.vintage = params[:vintage] || @wine.vintage
+    @wine.vintner = params[:vintner] || @wine.vintner
+    @wine.region = params[:region] || @wine.region
+    @wine.pairings = params[:pairings] || @wine.pairings
+    @wine.tasting_notes = params[:tasting_notes] || @wine.tasting_notes
+    @wine.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @wine = Wine.find_by(id: params[:id])
+    @wine.destroy
+    render json: {message: 'You have successfully deleted the wine from the cellar database.'}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     get "/wines" => "wines#index"
     get "/wines/:id" => "wines#show"
+    post "/wines" => "wines#create"
+    patch "/wines/:id" => "wines#update"
+    delete "wines/:id" => "wines#destroy"
   end
 end
