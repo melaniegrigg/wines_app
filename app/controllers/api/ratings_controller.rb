e0d1913d102d1b3f2439fedfbe0a6f74c8be00c5
melaniegrigg <melanie.grigger@gmail.com>
2020-09-01 21:16:18 -0400

    edit ratings display

--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -10,30 +10,29 @@ class Api::RatingsController < ApplicationController
   end
 
   def create
-    # p current_user
     @rating = Rating.new(
+      vintner: params[:vintner],
       rating: params[:rating],
       notes: params[:notes],
-      vintner: params[:vintner],)
-      user_id: current_user.id
-      if @post.save
-        render "show.json.jbuilder"
+      # user_id: current_user.id
+    )
+      if @rating.save
+        render "show.json.jb"
       else
-        render json: {errors: @post.errors.full_messages}, status: :bad_request
+        render json: {errors: @rating.errors.full_messages}, status: :bad_request
       end
-    @rating.user_wine.save
-    render "show.json.jb"
   end
 
   def update
     @rating = Rating.find_by(id: params[:id])
-    @rating.update(
-      rating: params[:rating],
-      notes: params[:notes],
-      vintner: params[:vintner],
-      user_wine: current_user.user_wine
-    )
-    render 'show.json.jb'
+    @rating.vintner = params[:vintner] || @rating.vintner
+    @rating.rating = params[:rating] || @rating.rating
+    @rating.notes = params[:notes] || @rating.notes
+    if @rating.save
+      render "show.json.jb"
+    else
+      render json: {errors: @rating.errors.full_messages}, status: :bad_request
+    end
   end
 
   def destroy
